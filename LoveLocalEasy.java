0992381e41ab7145011572072a6256139460408c
Safa Banu <143880461+Safa1219@users.noreply.github.com>
2023-12-05 04:46:22 -0800

    Update LoveLocalEasy.java

--- LoveLocalEasy.java
+++ LoveLocalEasy.java
@@ -1,9 +1,22 @@
+//Given a string s consisting of words and spaces, return the length of the last word in the string.
+//A word is a maximal 
+//substring consisting of non-space characters only.
+
 public class LoveLocalEasy {
-    public int lengthOfLastWord(String str) {
-        str = str.trim();
-        int lastSpaceIndex = str.lastIndexOf(32);
-        return str.length() - lastSpaceIndex - 1;
-    }
+        public int lengthOfLastWord(String s) {
+           
+            String str = s.trim();
+            int count = 0;
+            for(int i=str.length()-1;i>=0;i--){
+                if(str.charAt(i) != ' '){
+                    count++;
+                }else{
+                    break;
+                }
+            }
+            return count;
+        }
+
 
     public static void main(String[] args) {
         LoveLocalEasy a1 = new LoveLocalEasy();
@@ -12,3 +25,4 @@ public class LoveLocalEasy {
         System.out.println(a1.lengthOfLastWord("luffy is still joyboy"));
     }
 }
+
