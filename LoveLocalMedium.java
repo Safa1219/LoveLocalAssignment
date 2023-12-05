d2fb8cc3f7cd0bc1e07e21961e57e71a69ad4283
Safa Banu <143880461+Safa1219@users.noreply.github.com>
2023-12-05 04:47:45 -0800

    Update LoveLocalMedium.java

--- LoveLocalMedium.java
+++ LoveLocalMedium.java
@@ -1,63 +1,32 @@
+//Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
 public class LoveLocalMedium {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-
-        // In this step we will initialise all the candidate and their count
-        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
-
-        // First pass to find candidates
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
-            }
+        List<Integer> list = new ArrayList<Integer>();
+        int k = nums.length / 3 + 1, i;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            i = map.containsKey(num)? map.get(num) + 1 : 1;
+            map.put(num, i);
+            if(i == k)
+                list.add(num);
         }
-
-        // Second pass to verify candidates
-        count1 = count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            }
-        }
-
-        // Check if candidates have occurrences greater than n/3
-        if (count1 > nums.length / 3) {
-            result.add(candidate1);
-        }
-        if (count2 > nums.length / 3) {
-            result.add(candidate2);
-        }
-
-        return result;
+        return list;
     }
 
     public static void main(String[] args) {
         LoveLocalMedium solution = new LoveLocalMedium();
 
-        // Test cases
+
         int[] nums1 = {3, 2, 3};
         int[] nums2 = {1};
         int[] nums3 = {1, 2};
 
-        System.out.println(solution.majorityElement(nums1)); // Output: [3]
-        System.out.println(solution.majorityElement(nums2)); // Output: [1]
-        System.out.println(solution.majorityElement(nums3)); // Output: [1, 2]
+        System.out.println(solution.majorityElement(nums1));
+        System.out.println(solution.majorityElement(nums2));
+        System.out.println(solution.majorityElement(nums3));
     }
 }
 
